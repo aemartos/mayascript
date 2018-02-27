524aaed6ecd7fc9b8008371f1734b6c473cabc5d
aemartos <aemartos@gmail.com>
2018-02-27 19:12:27 +0100

    jQuery UI added (with example), sass reseted and es-maya dictionary included

--- js/main.js
+++ js/main.js
@@ -1,1 +1,12 @@
 
+$( function() {
+    $( "#draggable" ).draggable();
+    $( "#droppable" ).droppable({
+      drop: function( event, ui ) {
+        $( this )
+          .addClass( "ui-state-highlight" )
+          .find( "p" )
+            .html( "Dropped!" );
+      }
+    });
+  } );
