c5e0ce96610b18fa0cb862b5fec47eaee2b04141
aemartos <aemartos@gmail.com>
2018-03-16 18:07:10 +0100

    Grid included in game page, margins fixed

--- js/datos2.js
+++ js/datos2.js
@@ -5,6 +5,7 @@ data_game = [
 	"index" : 01,
 	"spanish" :"",
 	"mayan" :"",
+	"description":"",
 	"syllables" : {	
 		"syl01" : "",
 		"syl02" : "",
@@ -17,13 +18,46 @@ data_game = [
 	"index" : 02,
 	"spanish" :"",
 	"mayan" :"",
+	"description":"",
 	"syllables" : {	
-		"syl01" : "",
+		"syl01" : "pa",
 		"syl02" : "",
 		"syl03" : "",
 		"syl04" : "",
 		"syl05" : "",
 		}
 }
+
+];
+
+data_syl = [
+
+	"Q":"",
+	"q":"",
+	"pa":A",
+	"ba":a",
+ 	"no":S",
+ 	"ma":s",
+	"chu":D",
+ "bi":d",
+ "lu":F",
+n" e":f",
+ "ha":G",
+ "la":g",
+<k"ee":H",
+ "ku":h",
+ "hu":J",
+<k"aa":j",
+<c"ho":K",
+ "ho":k",
+ "ka":L",
+<c"ha":l",
+n" a":Ñ",
+ "ye":ñ",
+ "li":´",
+ "ke":¨",
+ "be":Ç",
+<c"hi":ç",
+
 
 ];
