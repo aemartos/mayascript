d9f1e950b35473aa3395bc79b2936c96eefa6804
Alejandro Albuerne <albuernearenasalejandro@gmail.com>
2018-03-20 23:11:30 +0100

    Codex json data formatted

--- js/codice.js
+++ js/codice.js
@@ -1,8 +1,23 @@
 codex = [
   {
-    "pronunciation" : "Sak ix",
-    "meaning" : "La Diosa Sak Ixchel",
-    "description" : "Era diosa del amor, de la gestación, del agua, de los trabajos textiles, de la luna y la medicina. Ixchel fue venerada como la diosa de la luna, por el carácter femenino de ésta. Representó la fertilidad estrechamente ligada con la tierra."
+    "pronunciation" : "",
+    "meaning" : "",
+    "description" : ""
+  },
+  {
+    "pronunciation" : "",
+    "meaning" : "",
+    "description" : ""
+  },
+  {
+    "pronunciation" : "",
+    "meaning" : "",
+    "description" : ""
+  },
+  {
+    "pronunciation" : "",
+    "meaning" : "",
+    "description" : ""
   },
   {
     "pronunciation" : "Sak ix",
@@ -10,18 +25,28 @@ codex = [
     "description" : "Era diosa del amor, de la gestación, del agua, de los trabajos textiles, de la luna y la medicina. Ixchel fue venerada como la diosa de la luna, por el carácter femenino de ésta. Representó la fertilidad estrechamente ligada con la tierra."
   },
   {
-    "pronunciation" : "Sak ix",
-    "meaning" : "La Diosa Sak Ixchel",
-    "description" : "Era diosa del amor, de la gestación, del agua, de los trabajos textiles, de la luna y la medicina. Ixchel fue venerada como la diosa de la luna, por el carácter femenino de ésta. Representó la fertilidad estrechamente ligada con la tierra."
+    "pronunciation" : "",
+    "meaning" : "",
+    "description" : ""
   },
   {
-    "pronunciation" : "Sak ix",
-    "meaning" : "La Diosa Sak Ixchel",
-    "description" : "Era diosa del amor, de la gestación, del agua, de los trabajos textiles, de la luna y la medicina. Ixchel fue venerada como la diosa de la luna, por el carácter femenino de ésta. Representó la fertilidad estrechamente ligada con la tierra."
+    "pronunciation" : "",
+    "meaning" : "",
+    "description" : ""
   },
   {
-    "pronunciation" : "Sak ix",
-    "meaning" : "La Diosa Sak Ixchel",
-    "description" : "Era diosa del amor, de la gestación, del agua, de los trabajos textiles, de la luna y la medicina. Ixchel fue venerada como la diosa de la luna, por el carácter femenino de ésta. Representó la fertilidad estrechamente ligada con la tierra."
+    "pronunciation" : "",
+    "meaning" : "",
+    "description" : ""
+  },
+  {
+    "pronunciation" : "",
+    "meaning" : "",
+    "description" : ""
+  },
+  {
+    "pronunciation" : "",
+    "meaning" : "",
+    "description" : ""
   }
 ];
